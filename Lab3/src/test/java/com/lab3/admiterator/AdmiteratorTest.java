0e2b01626b5f41df56cf8e89fbd6031591e33347
tud12193 <tud12193@temple.edu>
2015-03-25 10:41:54 -0400

    Lab3

---
+++ Lab3/src/test/java/com/lab3/admiterator/AdmiteratorTest.java
@@ -0,0 +1,20 @@
+package com.lab3.admiterator;
+
+import static org.junit.Assert.*;
+import junitparams.JUnitParamsRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+@RunWith(JUnitParamsRunner.class)
+
+public class AdmiteratorTest {
+ 
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	@Parameters(method = )
+
+}
